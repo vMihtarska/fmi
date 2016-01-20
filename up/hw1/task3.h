fddbeada3ad064e2d5d5d600f68aa073ebbf456f
vMihtarska <vasilkamihtarska@abv.bg>
2016-01-20 20:23:37 +0200

    Adding some task to my first up homework

---
+++ up/hw1/task3.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <cmath>
+
+int countDigits(int n) {
+	int count = 0;
+	while (n > 0) {
+		n = n / 10;
+		count++;
+	}
+
+	return count;
+}
+
+void automorphicNumbers() {
+	int n;
+	cout << "Enter a number: ";
+	cin >> n;
+	
+	int divisor = 0;
+	for (int i = 1; i < n; i++) {
+		divisor = pow(10, countDigits(i));
+		if ( ((i * i) % divisor) == i)
+			std::cout << i << "\n";
+	}
+}
